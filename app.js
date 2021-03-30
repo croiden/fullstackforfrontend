47651ace6d5df38b4c0ee3535a6a994964e2e127
Lawrence Croiden Lobo <llobo@planview.com>
2021-03-30 21:21:03 +0530

    Added custom header and status code

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack", "4life"); // custom header
+  res.status(418);
+  res.send("I prefer coffee...!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
